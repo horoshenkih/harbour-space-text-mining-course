bd950a236282c1ac7d12baf2b5eb74cb7b6321cd
khoroshenkikh-mipt <horoshenkih91@gmail.com>
2020-05-04 12:38:03 +0300

    no need in contextmanager, use `pre_run_cell` event hook

--- tmcourse/utils.py
+++ tmcourse/utils.py
@@ -42,13 +42,3 @@ def calendar_table(
         '</tr><tr>'.join('<td>{}</td>'.format('</td><td>'.join(str(_) for _ in row)) for row in table)
     )
     return html
-
-
-class colab_latex:
-    def __enter__(self):
-        exec("from google.colab.output._publish import javascript")
-        url = "https://colab.research.google.com/static/mathjax/MathJax.js?config=default"
-        javascript(url=url)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
