61ecf138b6fcc653bb3e0388792bcde9c4ca6f12
khoroshenkikh-mipt <horoshenkih91@gmail.com>
2020-05-17 16:09:11 +0300

    parametrize

--- tmcourse/utils.py
+++ tmcourse/utils.py
@@ -55,11 +55,13 @@ def calendar_table(
     return html
 
 
-def visualize_term_counter(term_counter, start, title, shift=10, font_size=22, figsize=(12, 8)):
+def visualize_term_counter(term_counter, start, title, shift=10, font_size=22, figsize=(12, 8), use_ggplot=True):
     import matplotlib.pyplot as plt
     plt.rcParams.update({'font.size': font_size})
 
-    plt.style.use('ggplot')
+    if use_ggplot:
+        plt.style.use('ggplot')
+
     plt.figure(figsize=figsize)
 
     ordered_term_count = term_counter.most_common()[start:start + shift]
